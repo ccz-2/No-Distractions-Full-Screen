cf9bc4ff922e86d73c046e18a1d224efa0643125
Quip13 <random.emailforcrap@gmail.com>
2020-03-12 01:38:43 -0400

    no longer applies multiple divs

--- bottom_bar.js
+++ bottom_bar.js
@@ -9,14 +9,20 @@ $("body").on('DOMSubtreeModified', 'td#middle', function() {
   clearTimeout(timer);
   timer = setTimeout(function(){
 	$('table:not([id="innertable"])').addClass("bar");
-	$('button[data-ease="1"]').closest('td').addClass('asdf').append('<div class="again"></div>')
-	$('button[data-ease="2"]').closest('td').addClass('asdf').append('<div class="hard"></div>')
-	$('button[data-ease="3"]').closest('td').addClass('asdf').append('<div class="good"></div>')
-	$('button[data-ease="4"]').closest('td').addClass('asdf').append('<div class="easy"></div>')
-	$("button").remove()
+	if (!$('.bar').hasClass('again')) {
+		$('button[data-ease="1"]').closest('td').addClass('asdf').append('<div class="again"></div>')
+		$('button[data-ease="2"]').closest('td').addClass('asdf').append('<div class="hard"></div>')
+		$('button[data-ease="3"]').closest('td').addClass('asdf').append('<div class="good"></div>')
+		$('button[data-ease="4"]').closest('td').addClass('asdf').append('<div class="easy"></div>')
+		$("button").remove()
+	}
+
   }, 10); //prevents overzealous updates, since selector grabs multiple events per card change
 });
 
+$("body").on('DOMSubtreeModified', 'div#bottomHover', function() {
+	console.log('modified')
+	});
 
 function getHeight() {
   height = $('table:not([id="innertable"])').height();
