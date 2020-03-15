9bc17dc72c3fb0558a8d7e2d7de6a36a2838c753
Quip13 <random.emailforcrap@gmail.com>
2020-03-15 00:35:38 -0400

    better colors

--- bottom_bar.js
+++ bottom_bar.js
@@ -6,23 +6,33 @@ $('body').append(`<div id="bottomHover"></div>`)
 //pretty bad - should not inject
 var timer;
 $("body").on('DOMSubtreeModified', 'td#middle', function() {
-  clearTimeout(timer);
-  timer = setTimeout(function(){
 	$('table:not([id="innertable"])').addClass("bar");
-	if (!$('.bar').hasClass('again')) {
-		$('button[data-ease="1"]').closest('td').addClass('asdf').append('<div class="again"></div>')
-		$('button[data-ease="2"]').closest('td').addClass('asdf').append('<div class="hard"></div>')
-		$('button[data-ease="3"]').closest('td').addClass('asdf').append('<div class="good"></div>')
-		$('button[data-ease="4"]').closest('td').addClass('asdf').append('<div class="easy"></div>')
-		$("button").remove()
+	if (!$('table.bar td div').hasClass('again')) {
+		$('button:contains("Again")').closest('td').append('<div class="again"></div>')
+		$('button:contains("Again")').remove()
 	}
-
-  }, 10); //prevents overzealous updates, since selector grabs multiple events per card change
+	if (!$('table.bar td div').hasClass('hard')) {
+		$('button:contains("Hard")').closest('td').append('<div class="hard"></div>')
+		$('button:contains("Hard")').remove()
+	}
+	if (!$('table.bar td div').hasClass('good')) {
+		$('button:contains("Good")').closest('td').append('<div class="good"></div>')
+		$('button:contains("Good")').remove()
+	}
+	if (!$('table.bar td div').hasClass('easy')) {
+		$('button:contains("Easy")').closest('td').append('<div class="easy"></div>')
+		$('button:contains("Easy")').remove()
+	}
+	if (!$('table.bar td div').hasClass('ans')){
+		$('button:contains("Show Answer")').closest('td').append('<div class="ans"></div>')
+		$('button:contains("Show Answer")').remove()
+	}
+	$('button').remove()
 });
 
-$("body").on('DOMSubtreeModified', 'div#bottomHover', function() {
-	console.log('modified')
-	});
+$("body").on('DOMContentLoaded', function() {
+	console.log(event.target)
+});
 
 function getHeight() {
   height = $('table:not([id="innertable"])').height();
