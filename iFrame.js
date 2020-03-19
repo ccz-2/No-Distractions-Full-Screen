43c8a5a71b9656e2de238d71fb4ad6a9d754283a
Quip13 <random.emailforcrap@gmail.com>
2020-03-18 22:27:35 -0400

    interact.js integration

--- iFrame.js
+++ iFrame.js
@@ -1,21 +1,38 @@
-//var op = 0.5; //Defined in python
-//var color = 'rgba(110, 110, 200, 0.8)';
+// NDFS v3.3
 
 var newheight;
 var newwidth;
-function autoResize(id){
-
-};
-
 
 $('body').append(`
-
+<div id="outer">
 <div class="bottomWrapper">
 <iframe id='bottomiFrame' frameborder="0" scrolling="no">
 </iframe>
 </div>
+</div>
 
 <style>
+
+#outer{
+  right: 0;
+  bottom: 0;
+  position: fixed;
+  left: 50%;
+  height: auto;
+  background-color: green;
+}
+
+.bottomWrapper {
+  position: relative;
+  left: -50%;
+  background-color: red;
+  margin: 0px;
+}
+
+</style>
+`);
+
+/*
 .bottomWrapper {
   text-align: center;
   position: fixed;
@@ -23,12 +40,9 @@ $('body').append(`
   left: 0;
   width: 100%;
 }
-
-</style>
-`);
+*/
 
 document.querySelector("iframe").addEventListener( "load", function(e) {
-
     var iframe = e.target
     var target = iframe.contentDocument.querySelector('table:not([id="innertable"])');
     newheight = target.scrollHeight;
@@ -37,4 +51,7 @@ document.querySelector("iframe").addEventListener( "load", function(e) {
     newwidth = (newwidth) + "px"
     iframe.height= newheight;
     iframe.width= newwidth;
+    $("div.bottomWrapper").outerHeight(newheight + 50);
+    $("div.bottomWrapper").outerWidth(newwidth);
+
 } );
