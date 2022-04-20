e1e6178027a8684cdaeb3ec64fad21d9fe9a4f0b
mobedoor <71059800+mobedoor@users.noreply.github.com>
2022-04-20 08:46:23 +0000

    Update toolbar.py
    updated with code from @ijgnd

--- toolbar.py
+++ toolbar.py
@@ -1,28 +1,24 @@
-# -*- coding: utf-8 -*-
-# Copyright (c) 2020 Lovac42
+# Copyright (c) 2020  Lovac42
+#           (c) 2021- ijgnd
 # License: GNU AGPL, version 3 or later; http://www.gnu.org/licenses/agpl.html
 
 
 from aqt import QMenu
 
-def getMenu(parent, menuName):
-    menu = None
-    for a in parent.form.menubar.actions():
-        if menuName == a.text():
-            menu = a.menu()
-            break
-    if not menu:
-        menu = parent.form.menubar.addMenu(menuName)
-    return menu
+
+def getMenu(parent, menu_name):
+    menubar = parent.form.menubar
+    for a in menubar.actions():
+        if menu_name == a.text():
+            return a.parent()
+    else:
+        return menubar.addMenu(menu_name)
 
 
-def getSubMenu(menu, subMenuName):
-    subMenu = None
-    for a in menu.actions():
-        if subMenuName == a.text():
-            subMenu = a.menu()
-            break
-    if not subMenu:
-        subMenu = QMenu(subMenuName, menu)
-        menu.addMenu(subMenu)
-    return subMenu
+def getAction(parent, actionName):
+    menubar = parent.form.menubar
+    for a in menubar.actions():
+        if actionName == a.text():
+            return a
+    else:
+        return menubar.addAction(actionName)
